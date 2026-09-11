14965d50eeacd295bab723d638e05bc02ac7c7e3
unknown <aaronjohnparonda@gmail.com>
2026-09-11 17:51:42 +0800

    Created the contents of the CustomerDto.cs

--- SimpleInventorySystem/Model/DTO/CustomerDto.cs
+++ SimpleInventorySystem/Model/DTO/CustomerDto.cs
@@ -2,5 +2,8 @@
 {
     public class CustomerDto
     {
+        public int CustomerId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Address { get; set; }
     }
 }
